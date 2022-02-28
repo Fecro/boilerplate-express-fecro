dad6166566452beddbf584d1e03ee2dede2c51f3
Franklin <fecr88@gmail.com>
2022-02-28 18:54:40 +0000

    Use the .env File - Doing This Challenge

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
+var absolutePath = __dirname + "/views/index.html";
+var stylePath = __dirname + "/public";
+app.use("/public", express.static(stylePath));
 app.get("/", (req, res) => {
-  res.send("Hello Express")
+  res.sendFile(absolutePath);
+});
+app.get("/json", (req, res) => {
+  res.json({
+    "message": "Hello json"
+  });
 });
 
 
