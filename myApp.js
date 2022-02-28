b5e67e9a275bdcf49be5f60971073a14ea2753f3
Franklin <fecr88@gmail.com>
2022-02-28 19:06:33 +0000

    I finished the last challenge.

--- myApp.js
+++ myApp.js
@@ -7,9 +7,15 @@ app.get("/", (req, res) => {
   res.sendFile(absolutePath);
 });
 app.get("/json", (req, res) => {
-  res.json({
-    "message": "Hello json"
+  if (process.env.MESSAGE_STYLE==="uppercase"){
+    res.json({
+      "message": "HELLO JSON"
+    });
+  } else {
+    res.json({
+      "message": "Hello json"
   });
+  }
 });
 
 
